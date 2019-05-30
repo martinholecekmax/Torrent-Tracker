229b4d5c02f8e76b6b5d66ebf7b9688d17ab672d
100344605@unimail.derby.ac.uk <100344605@unimail.derby.ac.uk>
2019-05-30 13:33:10 +0100

    Security alert

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -11,7 +11,7 @@ try {
     window.$ = window.jQuery = require('jquery');
 
     require('bootstrap-sass');
-} catch (e) {}
+} catch (e) { }
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -19,7 +19,7 @@ try {
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
+window.axios = require('vue-axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
